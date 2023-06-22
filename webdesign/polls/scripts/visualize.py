03c4ee0f470e9de49e2bfc62c361b5c36cda7c66
ajs997 <ajaysanjeevan@gmail.com>
2023-06-22 13:07:46 -0400

    headless mode update

--- webdesign/polls/scripts/visualize.py
+++ webdesign/polls/scripts/visualize.py
@@ -121,11 +121,28 @@ def single_ts_trend_plot(pp_ts,trend_ts,cat_ts,bin_bounds, title=None):  ## use 
     plt.close(f)  
     return name 
 
-def single_ts_level_plot_nosave(pp_ts,cat_ts,bin_bounds):  ## use for levels
-    label = cat_ts.values[0][0]
+def multi_signal_plot_nosave(cat_df, cat_method):
+    label = [x[0] for x in cat_df.values.flatten()][0]
     
     f,ax = plt.subplots(figsize=(20,8),facecolor='white')
-    plt.title('Single time series - Level-based categories',fontsize=20)
+    if cat_method[0]=='categorizetypelevel':
+        cat_label = 'Level-based'
+    else:
+        cat_label = 'Trend-based'
+    plt.title('Multi signal - {} categories'.format(cat_label))
+    plt.ylabel('Categorical time series ({})'.format(label))
+    
+    cat_df.applymap(lambda x: int(x[1:])).plot(ax=ax,lw=2,drawstyle='steps')
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+def single_ts_level_plot_nosave(pp_ts,cat_ts,bin_bounds,title=None):  ## use for levels
+    label = cat_ts.values[0][0]
+    f,ax = plt.subplots(figsize=(20,8),facecolor='white')
+    if title:
+        plt.title(title+' - Level-based categories',fontsize=20)
+    else:
+        plt.title('Single time series - Level-based categories',fontsize=20)
+        
     ax = plt.gca()
     
     pp_ts.plot(x='date',y='value',ax=ax,label='Preprocessed time series',color='C0',legend=False)
@@ -136,7 +153,7 @@ def single_ts_level_plot_nosave(pp_ts,cat_ts,bin_bounds):  ## use for levels
     ax.set_ylabel('Time series values',color='C0')
 
     ax2 = ax.twinx()
-    cat_ts.apply(lambda x: int(x[1:])).astype(int).plot(ax=ax2,color='k',lw=2,drawstyle='steps',label='Categorical time series')
+    cat_ts.apply(lambda x: int(x[1:])).astype(int).plot(ax=ax2,drawstyle='steps',color='k',lw=2,label='Categorical time series')
     ax2.spines['left'].set_color('C0')
     ax2.spines['right'].set_linewidth(2)
     ax2.yaxis.set_major_locator(MaxNLocator(integer=True))
@@ -153,11 +170,14 @@ def single_ts_level_plot_nosave(pp_ts,cat_ts,bin_bounds):  ## use for levels
     # Add the second legend as usual
     ax2.legend(handles2, labels2,loc=1)
 
-def single_ts_trend_plot_nosave(pp_ts,trend_ts,cat_ts,bin_bounds):  ## use for trends  
+def single_ts_trend_plot_nosave(pp_ts,trend_ts,cat_ts,bin_bounds,title=None):  ## use for trends  
     label = cat_ts.values[0][0]
     
     f,ax = plt.subplots(figsize=(20,8),facecolor='white')
-    plt.title('Single time series - Trend-based categories',fontsize=20)
+    if title:
+        plt.title(title+' - Level-based categories',fontsize=20)
+    else:
+        plt.title('Single time series - Trend-based categories',fontsize=20)
     
     ax = plt.gca()
     pp_ts.plot(x='date',y='value',ax=ax,label='Preprocessed time series',color='C0',legend=False)
@@ -175,7 +195,7 @@ def single_ts_trend_plot_nosave(pp_ts,trend_ts,cat_ts,bin_bounds):  ## use for t
     ax2.set_ylabel('Trend time series',color='C1')
     
     ax3 = ax.twinx()
-    cat_ts.apply(lambda x: int(x[1:])).astype(int).plot(ax=ax3,color='k',lw=2,drawstyle='steps',label='Categorical time series')
+    cat_ts.apply(lambda x: int(x[1:])).astype(int).plot(ax=ax3,drawstyle='steps',color='k',lw=2,label='Categorical time series')
     ax3.spines["right"].set_position(("axes", 1.1))
     ax3.yaxis.set_major_locator(MaxNLocator(integer=True))
     ax3.spines['right'].set_linewidth(2)
